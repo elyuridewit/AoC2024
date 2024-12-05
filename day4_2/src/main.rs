7ce00e26fdd6b65fc92118a4bf27c0999acb52ea
Yuri <yuri@snergang.org>
2024-12-05 02:00:09 +0100

    Day 4 complete!

--- day4_2/src/main.rs
+++ day4_2/src/main.rs
@@ -1,5 +1,25 @@
 use std::{env, fs};
-use ndarray::Array2;
+
+
+struct MasCheck {
+    matrix: Vec<Vec<char>>
+}
+impl MasCheck {
+    fn mas_checker(&self, yidx: usize, xidx: usize) -> bool {
+        let topleft = self.matrix[yidx-1][xidx-1];
+        let bottomright = self.matrix[yidx+1][xidx+1];
+        let bottomleft = self.matrix[yidx+1][xidx-1];
+        let topright = self.matrix[yidx-1][xidx+1];
+        // Topleft -> bottomright
+
+        if (topleft == 'M' && bottomright == 'S' || topleft == 'S' && bottomright == 'M') && (bottomleft == 'M' && topright == 'S' || bottomleft == 'S' && topright == 'M') {
+            return true
+        }
+        false
+    }
+}
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,70 +35,24 @@ fn main() {
         jagged_vec.push(line_vec);
     }
     
-    let xbound = jagged_vec[0].len();
-    let mut array: Array2<char> = Array2::from_elem((jagged_vec.len(), xbound), ' ');
-
-    for (i, row) in jagged_vec.iter().enumerate() { 
-        for (j, &val) in row.iter().enumerate() { 
-            array[(i, j)] = val; 
-        } 
-    }
-
-    // Get horizontals
-    let mut horizontals: Vec<Vec<char>> = (0..array.nrows()).map(|i| {
-        array.row(i).to_vec()
-    }).collect();
-
-    // Get verticals
-    let mut verticals: Vec<Vec<char>> = (0..array.ncols()).map(|j| {
-        array.column(j).to_vec()
-    }).collect();
-
-    // Get diagonals (from top-left to bottom-right and top-right to bottom-left)
-    let mut diag1: Vec<Vec<char>> = Vec::new();
-    let mut diag2: Vec<Vec<char>> = Vec::new();
+    let xbound = jagged_vec[0].len() - 1;
+    let ybound = jagged_vec.len() - 1;
+    let mas_check: MasCheck = MasCheck{
+        matrix: jagged_vec.clone()
+    };
     
-    for k in 0..(array.nrows() + array.ncols() - 1) {
-        let mut d1 = Vec::new();
-        let mut d2 = Vec::new();
-        
-        for i in 0..array.nrows() {
-            let j = k as isize - i as isize;
-            if j >= 0 && j < array.ncols() as isize {
-                d1.push(array[(i, j as usize)]);
-            }
-
-            let j = i as isize + array.ncols() as isize - 1 - k as isize;
-            if j >= 0 && j < array.ncols() as isize {
-                d2.push(array[(i, j as usize)]);
-            }
-        }
-
-        if !d1.is_empty() {
-            diag1.push(d1);
-        }
-        if !d2.is_empty() {
-            diag2.push(d2);
-        }
-    }
-
-    let mut all_windows: Vec<Vec<char>> = Vec::new();
-    all_windows.append(&mut horizontals);
-    all_windows.append(&mut verticals);
-    all_windows.append(&mut diag1);
-    all_windows.append(&mut diag2);
-
     let mut total: usize = 0;
-    
-    let xmas = "XMAS";
-    let samx = "SAMX";
-    for window in all_windows {
-        let line_str: String = window.iter().collect();
-        if line_str.contains(xmas) || line_str.contains(samx) {
-            total += line_str.as_bytes().windows(xmas.len()).filter(|&w| w == xmas.as_bytes()).count();
-            total += line_str.as_bytes().windows(samx.len()).filter(|&w| w == samx.as_bytes()).count();
+    for (yidx, line) in jagged_vec.into_iter().enumerate() {
+        for (xidx, char) in line.into_iter().enumerate() {
+            if yidx != 0 && xidx != 0 && yidx < ybound && xidx < xbound {
+                if char == 'A' {
+                    if mas_check.mas_checker(yidx, xidx) {
+                        total += 1;
+                    }
+                }
+            }
         }
     }
-    println!("Total occurences: {:?}", total)
 
+    println!("Total: {:?}", total)
 }
